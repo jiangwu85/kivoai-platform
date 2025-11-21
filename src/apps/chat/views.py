7bbcaea6ec286462fae4835b3f6749fd4826ff25
jw <273617974@qq.com>
2025-11-22 01:18:53 +0800

    init 10

--- src/apps/chat/views.py
+++ src/apps/chat/views.py
@@ -7,7 +7,7 @@ app = APIRouter()
 
 
 @app.get("/", summary="聊天室接口状态")
-def app_root(version: str):
+async def app_root(version: str):
     data = {
         "当前时间": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
         "test": "聊天室",
@@ -16,7 +16,7 @@ def app_root(version: str):
     return SuccessResponse(data=data)
 
 @app.get("/test", summary="test API")
-def app_root(version: str):
+async def app_root(version: str):
     data = {
         "当前时间": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
         "test": "聊天室",
--- src/apps/tasks/views.py
+++ src/apps/tasks/views.py
@@ -7,7 +7,7 @@ app = APIRouter()
 
 
 @app.get("/", summary="后台任务接口状态")
-def app_root(version: str):
+async def app_root(version: str):
     data = {
         "当前时间": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
         "test": "后台任务",
@@ -17,7 +17,7 @@ def app_root(version: str):
 
 
 @app.get("/list", summary="后台任务列表")
-def get_tasks(version: str):
+async def get_tasks(version: str):
     with open("scripts/tasks.json", 'r', encoding="utf-8") as f:
         data = json.load(f)
     # TODO 改为数据库模式，因为每天更新数据后，需要改变更新事件，数据库更加方便
