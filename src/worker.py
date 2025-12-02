8b6337c85d665495ccbfd3525e2e1aea0e57dfb8
jw <273617974@qq.com>
2025-12-02 23:34:33 +0800

    init 19

--- src/worker.py
+++ src/worker.py
@@ -66,10 +66,12 @@ async def db(req: Request):
     env = req.scope["env"]
     print("--------1--------")
     results = await env.DB.prepare(query).all()
+    print(results)
     print("--------2--------")
     data = results.results[0]
-    print("--------3--------")
     print(data)
+    print("--------3--------")
+
     return {"message": data}
 
 
