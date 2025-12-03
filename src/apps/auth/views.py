6c8006ac0a6fbfcba78787160c7cfa461431635b
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-03 18:46:35 +0800

    init 03

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -8,7 +8,7 @@ app = APIRouter()
 
 
 @app.post("/register", summary="register")
-async def app_root(req: Request,register: Register):
+async def register(req: Request,reg: Register):
     env = req.scope["env"]
     results = await env.DB.prepare('INSERT INTO "main"."user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
     #results = results.results
@@ -22,12 +22,35 @@ async def app_root(req: Request,register: Register):
     return SuccessResponse(data=data)
 
 @app.post("/login", summary="register")
-async def app_root(req: Request,login: Login):
+async def login(req: Request,lg: Login):
     env = req.scope["env"]
-    results = await env.DB.prepare('select * from user where email=?').bind(reg.email).run()
+    results = await env.DB.prepare('select * from user where email=?').bind(lg.email).run()
     result = results.to_py()
     # if(result == False):
     #     return ErrorResponse(code=401,data="User password incorrect!")
+    data = {
+        "user": result,
+        "accessToken": "1",
+        "refreshToken": "1",
+        "expiresDateTime": time.time_ns(),
+    }
+    return SuccessResponse(data=data)
+
+
+@app.post("/register1", summary="register")
+async def register1(req: Request,reg: Register):
+    result = common.register(req,reg)
+    data = {
+        "user": result,
+        "accessToken": "1",
+        "refreshToken": "1",
+        "expiresDateTime": time.time_ns(),
+    }
+    return SuccessResponse(data=data)
+
+@app.post("/login1", summary="register")
+async def login1(req: Request,lg: Login):
+    result = common.login(req,lg)
     data = {
         "user": result,
         "accessToken": "1",
--- src/crud/common.py
+++ src/crud/common.py
@@ -1,18 +1,18 @@
 
 from fastapi import Request
 from core.moudles import Register,Login
-def register(req: Request,reg: Register):
+async def register(req: Request,reg: Register):
     env = req.scope["env"]
-    results = env.DB.prepare('INSERT INTO "main"."user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
-    #results = results.results
+    results = await env.DB.prepare('INSERT INTO "main"."user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
+    results = results.results
     results = results.to_py()
     return results
 
-def login(req: Request,reg: Login):
+async def login(req: Request,lg: Login):
     env = req.scope["env"]
-    results = env.DB.prepare('select * from user where email=?').bind(reg.email).run()
+    results = await env.DB.prepare('select * from user where email=?').bind(lg.email).run()
     #results = results.results
-    #results = results.to_py()
+    results = results.to_py()
     # if len(results)==1:
     #     return results[0]
     return results
