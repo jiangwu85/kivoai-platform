0960ae6a463814bbb67f0fb033bc3fe370652b15
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-03 19:15:18 +0800

    init 03

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -21,52 +21,10 @@ async def register(req: Request,reg: Register):
     }
     return SuccessResponse(data=data)
 
-@app.post("/login", summary="register")
-async def login(req: Request,lg: Login):
-    env = req.scope["env"]
-    results = await env.DB.prepare('select * from user where email=?').bind(lg.email).run()
-    results = results.results
-    result = results.to_py()
-    # if(result == False):
-    #     return ErrorResponse(code=401,data="User password incorrect!")
-    data = {
-        "user": result,
-        "accessToken": "1",
-        "refreshToken": "1",
-        "expiresDateTime": time.time_ns(),
-    }
-    return SuccessResponse(data=data)
-@app.post("/login0", summary="register")
-async def login0(req: Request,lg: Login):
-    env = req.scope["env"]
-    results = await env.DB.prepare("select * from user").run()
-    results = results.results
-    result = results.to_py()
-    # if(result == False):
-    #     return ErrorResponse(code=401,data="User password incorrect!")
-    data = {
-        "user": result,
-        "accessToken": "1",
-        "refreshToken": "1",
-        "expiresDateTime": time.time_ns(),
-    }
-    return SuccessResponse(data=data)
-
 
 @app.post("/register1", summary="register")
 async def register1(req: Request,reg: Register):
     result = common.register(req,reg)
-    data = {
-        "user": result,
-        "accessToken": "1",
-        "refreshToken": "1",
-        "expiresDateTime": time.time_ns(),
-    }
-    return SuccessResponse(data=data)
-
-@app.post("/login1", summary="register")
-async def login1(req: Request,lg: Login):
-    result = common.login(req,lg)
     data = {
         "user": result,
         "accessToken": "1",
@@ -83,11 +41,16 @@ async def db1(req: Request):
     results = results.to_py()
     # Return a JSON response
     return {"code": 200,"message": "success","data": results}
-@app.get("/db2")
-async def db2(req: Request):
+@app.get("/login")
+async def login(req: Request,lg: Login):
     env = req.scope["env"]
-    results = await env.DB.prepare("select * from user where email=?").bind("273617974@qq.com").run()
+    results = await env.DB.prepare("select * from user where email=?").bind(lg.email).run()
     results = results.results
-    results = results.to_py()
-    # Return a JSON response
-    return {"code": 200,"message": "success","data": results}
+    result = results.to_py()
+    data = {
+        "user": result,
+        "accessToken": "1",
+        "refreshToken": "1",
+        "expiresDateTime": time.time_ns(),
+    }
+    return {"code": 200,"message": "success","data": data}
