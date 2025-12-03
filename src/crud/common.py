b4668283f7bf98363728f2b5ea9b37a852305c8e
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-03 18:33:31 +0800

    init 01

--- src/crud/common.py
+++ src/crud/common.py
@@ -4,14 +4,14 @@ from core.moudles import Register,Login
 def register(req: Request,reg: Register):
     env = req.scope["env"]
     results = env.DB.prepare('INSERT INTO "main"."user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
-    results = results.results
+    #results = results.results
     results = results.to_py()
     return results
 
 def login(req: Request,reg: Login):
     env = req.scope["env"]
     results = env.DB.prepare('select * from user where email=?').bind(reg.email).run()
-    results = results.results
+    #results = results.results
     results = results.to_py()
     # if len(results)==1:
     #     return results[0]
