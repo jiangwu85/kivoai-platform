1d7ed47e3e1a7bc2f0d4c9b36fc25362a57ceeb4
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-03 19:06:21 +0800

    init 01

--- src/crud/common.py
+++ src/crud/common.py
@@ -3,7 +3,7 @@ from fastapi import Request
 from core.moudles import Register,Login
 async def register(req: Request,reg: Register):
     env = req.scope["env"]
-    results = await env.DB.prepare('INSERT INTO "main"."user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
+    results = await env.DB.prepare("INSERT INTO user (email, password, status, role, firstName, lastName, phone) VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *").bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
     results = results.results
     results = results.to_py()
     return results
