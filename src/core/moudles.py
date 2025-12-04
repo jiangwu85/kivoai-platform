435b00613a83963830caed310cc1f2824a853bfe
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 14:36:48 +0800

    init 00

--- src/core/moudles.py
+++ src/core/moudles.py
@@ -13,15 +13,12 @@ class Register(BaseModel):
     birthDate: Optional[str] = Field(default="")
     location: Optional[str] = Field(default="")
     bio: Optional[str] = Field(default="")
-
-    @field_validator("email")
-    @classmethod
-    def validate_email(self, value):
-        return vali_email(value)
+    # @field_validator('email')
+    # def validate_email(self, value):
+    #     return vali_email(value)
 class Login(BaseModel):
     email: str = Field(min_length=5,  default="273617974@qq.com")
     password: str = Field(min_length=6, default="123456")
-    @field_validator("email")
-    @classmethod
-    def validate_email(self, value):
-        return vali_email(value)
+    # @field_validator("email")
+    # def validate_email(self, value):
+    #     return vali_email(value)
