ef2dbe9a4786db2be5f6c55d77fec1289f6bdfd9
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 16:39:40 +0800

    init 01

--- src/core/Auth.py
+++ src/core/Auth.py
@@ -2,10 +2,6 @@
 from fastapi import Request, Header, HTTPException
 
 # 鉴权依赖项
-async def authenticate(req: Request, token: str = Header(..., alias="Authorization")):
+async def authenticate(token: str = Header(..., alias="Authorization")):
     print("Authenticating token:", token)
-    env = req.scope["env"]
-    val = await env.REDIS.get(token)
-    if val is None:
-        raise HTTPException(status_code=401, detail="Unauthorized access")
     return token
