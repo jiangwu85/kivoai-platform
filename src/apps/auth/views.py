a133cb85c84c2f80698e16786c16d48b052aa776
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 11:51:17 +0800

    init 08

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -4,8 +4,6 @@ from fastapi import APIRouter,Request
 from fastapi.encoders import jsonable_encoder
 from utils.response import SuccessResponse,ErrorResponse
 from core.moudles import Register,Login
-from crud import common
-import orjson
 
 app = APIRouter()
 
@@ -14,7 +12,7 @@ app = APIRouter()
 async def register(req: Request,reg: Register):
     env = req.scope["env"]
     results = await env.DB.prepare('INSERT INTO "user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
-    results = results.results
+    results = results.results[0]
     result = results.to_py()
     result = jsonable_encoder(result)
     data = {
@@ -25,59 +23,11 @@ async def register(req: Request,reg: Register):
     }
     return SuccessResponse(data=data)
 
-
-@app.post("/register1", summary="register")
-async def register1(req: Request,reg: Register):
-    result = common.register(req,reg)
-    data = {
-        "user": result,
-        "accessToken": "1",
-        "refreshToken": "1",
-        "expiresDateTime": time.time_ns(),
-    }
-    return SuccessResponse(data=data)
-
-@app.get("/db")
-async def db(req: Request):
-    env = req.scope["env"]
-    results = await env.DB.prepare("select * from user").run()
-    results = results.results
-    results = results.to_py()
-    json_compatible_data = jsonable_encoder(results)
-    # Return a JSON response
-    return SuccessResponse(data=json_compatible_data)
-@app.get("/db0")
-async def db0(req: Request):
-    env = req.scope["env"]
-    results = await env.DB.prepare("select * from user").run()
-    results = results.results[0]
-    results = results.to_py()
-    json_compatible_data = jsonable_encoder(results)
-    # Return a JSON response
-    return SuccessResponse(data=json_compatible_data)
-@app.get("/db1")
-async def db1(req: Request):
-    env = req.scope["env"]
-    results = await env.DB.prepare("select * from user where email='273617974@qq.com'").run()
-    results = results.results
-    results = results.to_py()
-    json_compatible_data = jsonable_encoder(results)
-    # Return a JSON response
-    return SuccessResponse(data=json_compatible_data)
-@app.get("/db2")
-async def db2(req: Request):
-    env = req.scope["env"]
-    results = await env.DB.prepare("select * from user where email=?").bind("273617974@qq.com").run()
-    results = results.results
-    results = results.to_py()
-    json_compatible_data = jsonable_encoder(results)
-    # Return a JSON response
-    return SuccessResponse(data=json_compatible_data)
 @app.post("/login")
 async def login(req: Request,lg: Login):
     env = req.scope["env"]
     results = await env.DB.prepare("select * from user where email=?").bind(lg.email).run()
-    result = results.results
+    result = results.results[0]
     result = jsonable_encoder(result.to_py())
     data = {
         "user": result,
@@ -86,17 +36,3 @@ async def login(req: Request,lg: Login):
         "expiresDateTime": time.time(),
     }
     return SuccessResponse(data=data)
-
-@app.post("/login1")
-async def login1(req: Request,lg: Login):
-    env = req.scope["env"]
-    results = await env.DB.prepare("select * from user where email=?").bind(lg.email).run()
-    results = results.results
-    result = results.to_py()
-    data = {
-        "user": result,
-        "accessToken": "1",
-        "refreshToken": "1",
-        "expiresDateTime": time.time_ns(),
-    }
-    return {"code": 200,"message": "success","data": data}
