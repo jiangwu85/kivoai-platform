de424d408158bb681a216c1fb0d99a1a51ed7a0b
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 13:02:48 +0800

    init 02

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -22,7 +22,7 @@ async def register(req: Request,reg: Register):
 
 @app.post("/login")
 async def login(req: Request,lg: Login):
-    result = common.register(req.scope["env"],lg)
+    result = await common.register(req.scope["env"],lg)
     data = {
         "user": result,
         "accessToken": "1",
@@ -31,33 +31,3 @@ async def login(req: Request,lg: Login):
     }
     return SuccessResponse(data=data)
 
-
-
-@app.post("/register1", summary="register")
-async def register1(req: Request,reg: Register):
-    env = req.scope["env"]
-    results = await env.DB.prepare('INSERT INTO "user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
-    results = results.results[0]
-    result = results.to_py()
-    result = jsonable_encoder(result)
-    data = {
-        "user": result,
-        "accessToken": "1",
-        "refreshToken": "1",
-        "expiresDateTime": time.time(),
-    }
-    return SuccessResponse(data=data)
-
-@app.post("/login1")
-async def login1(req: Request,lg: Login):
-    env = req.scope["env"]
-    results = await env.DB.prepare("select * from user where email=?").bind(lg.email).run()
-    result = results.results[0]
-    result = jsonable_encoder(result.to_py())
-    data = {
-        "user": result,
-        "accessToken": "1",
-        "refreshToken": "1",
-        "expiresDateTime": time.time(),
-    }
-    return SuccessResponse(data=data)
--- src/crud/common.py
+++ src/crud/common.py
@@ -2,8 +2,8 @@
 from fastapi.encoders import jsonable_encoder
 from core.moudles import Register,Login
 from typing import Any
-async def register(env: Any,reg: Register):
-    results = await env.DB.prepare('INSERT INTO "user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
+def register(env: Any,reg: Register):
+    results = env.DB.prepare('INSERT INTO "user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
     results = results.results[0]
     result = results.to_py()
     return jsonable_encoder(result)
