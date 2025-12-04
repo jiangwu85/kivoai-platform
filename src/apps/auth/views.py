88a96fea9d6d21b3e4b09d7d771b0a49538ee9d4
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 12:58:28 +0800

    init 01

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -30,3 +30,34 @@ async def login(req: Request,lg: Login):
         "expiresDateTime": time.time(),
     }
     return SuccessResponse(data=data)
+
+
+
+@app.post("/register1", summary="register")
+async def register1(req: Request,reg: Register):
+    env = req.scope["env"]
+    results = await env.DB.prepare('INSERT INTO "user" ("email", "password", "status", "role", "firstName", "lastName", "phone") VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
+    results = results.results[0]
+    result = results.to_py()
+    result = jsonable_encoder(result)
+    data = {
+        "user": result,
+        "accessToken": "1",
+        "refreshToken": "1",
+        "expiresDateTime": time.time(),
+    }
+    return SuccessResponse(data=data)
+
+@app.post("/login1")
+async def login1(req: Request,lg: Login):
+    env = req.scope["env"]
+    results = await env.DB.prepare("select * from user where email=?").bind(lg.email).run()
+    result = results.results[0]
+    result = jsonable_encoder(result.to_py())
+    data = {
+        "user": result,
+        "accessToken": "1",
+        "refreshToken": "1",
+        "expiresDateTime": time.time(),
+    }
+    return SuccessResponse(data=data)
