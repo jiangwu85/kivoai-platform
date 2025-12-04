a756718e7edb415415de37ce68d8587d71513900
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 13:22:38 +0800

    init 05

--- src/core/moudles.py
+++ src/core/moudles.py
@@ -4,10 +4,14 @@ from typing import Optional
 class Register(BaseModel):
     role: int
     email: str
+    password: str
     firstName: str
     lastName: str
+    gender: Optional[str] = None
     phone: Optional[str] = None
-    password: str
+    birthDate: Optional[str] = None
+    location: Optional[str] = None
+    bio: Optional[str] = None
 class Login(BaseModel):
     email: str
     password: str
--- src/crud/common.py
+++ src/crud/common.py
@@ -3,13 +3,13 @@ from fastapi.encoders import jsonable_encoder
 from core.moudles import Register,Login
 from typing import Any
 async def register(env: Any,reg: Register):
-    results = await env.DB.prepare('INSERT INTO user (email, password, status, role, firstName, lastName, phone) VALUES(?,?, 9, ?, ?, ?, ?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.phone).run()
+    results = await env.DB.prepare('INSERT INTO user (email,password,status,role,firstName,lastName,gender, phone,birthDate,location,bio) VALUES(?,?,9,?,?,?, ?,?,?,?,?) RETURNING *').bind(reg.email,reg.password,reg.role,reg.firstName,reg.lastName,reg.gender,reg.phone,reg.birthDate,reg.location,reg.bio).run()
     results = results.results[0]
     result = results.to_py()
     return jsonable_encoder(result)
 
 async def login(env: Any,lg: Login):
-    results = await env.DB.prepare("select * from user where email=?").bind(lg.email).run()
+    results = await env.DB.prepare("select email,status,role,firstName,lastName,gender,phone,birthDate,location,bio from user where email=?").bind(lg.email).run()
     result = results.results[0]
     return jsonable_encoder(result.to_py())
 
