d34bb929493cba3ea4dab18c781effc476c7f65c
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 17:30:56 +0800

    init 07

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -1,6 +1,7 @@
 import time
+from typing import Optional
 
-from fastapi import APIRouter,Request
+from fastapi import APIRouter, Request, HTTPException, status, Depends
 from fastapi.encoders import jsonable_encoder
 from utils.response import SuccessResponse,ErrorResponse
 from core.moudles import Register,Login
@@ -28,6 +29,27 @@ async def login(req: Request,lg: Login):
         "lg": jsonable_encoder(lg),
         "user": result,
         "accessToken": "1",
+        "refreshToken": "1",
+        "expiresDateTime": time.time(),
+    }
+    return SuccessResponse(data=data)
+
+def get_access_token(token: Optional[str] = None, authorization: Optional[str] = None):
+    if token:
+        return token  # 返回X-Token头部的值
+    elif authorization:
+        bearer_token = authorization.split(" ")[1]  # 假设是"Bearer <token>"格式
+        return bearer_token  # 返回Authorization头部的值
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid authentication credentials"
+        )
+
+@app.get("/me")
+async def me(accessToken: str = Depends(get_access_token)):
+    data = {
+        "accessToken": accessToken,
         "refreshToken": "1",
         "expiresDateTime": time.time(),
     }
