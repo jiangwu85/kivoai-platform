d8a29211cdb3aa4883854b832d59c097c52bbc83
Wu Jiang <Wu_Jiang@swissre.com>
2025-12-04 17:04:09 +0800

    init 02

--- src/application/urls.py
+++ src/application/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     {"ApiRouter": chat, "prefix": "/api/{version}/ws", "tags": ["聊天室"]},
     {"ApiRouter": tasks, "prefix": "/api/{version}/tasks", "tags": ["后台任务"]},
     {"ApiRouter": auth, "prefix": "/api/auth", "tags": ["Common API"]},
-    {"ApiRouter": auth, "prefix": "/api/test", "tags": ["Common API"]},
+    {"ApiRouter": token, "prefix": "/api/test", "tags": ["Common API"]},
 ]
